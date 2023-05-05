37224c1eaf5e02346ebb20b5a600c014b92e9f38
Josh Scanlan <75971516+joshuascan@users.noreply.github.com>
2023-05-05 10:45:49 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Magic Auth Facebook Social Login
+
+Please [click here](https://magic.link/docs/auth/login-methods/social-logins/integration/social-providers/facebook) for full implementation instructions
---
+++ src/pages/Login.js
@@ -0,0 +1,27 @@
+import { magic } from "../lib/magic";
+import { FaFacebook } from "react-icons/fa";
+
+const Login = () => {
+  const handleSocialLogin = async () => {
+    try {
+      await magic.oauth.loginWithRedirect({
+        provider: "facebook",
+        redirectURI: new URL("/dashboard", window.location.origin).href,
+      });
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  return (
+    <div className="container">
+      <h1>Magic Auth + Facebook</h1>
+      <button onClick={handleSocialLogin}>
+        <FaFacebook size={"2.5rem"} />
+        Log in with Facebook
+      </button>
+    </div>
+  );
+};
+
+export default Login;
